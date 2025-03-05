a7c63d30995d39b8ca15062bc2b189637531ede1
Cezary Dryl <cezary.dryl@pagepro.pl>
2025-03-05 15:11:21 +0100

    clg

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
 import NextErrorComponent, { ErrorProps } from "next/error";
 
-const CustomErrorPage: NextPage<ErrorProps> = (props) => (
-  <NextErrorComponent {...props} withDarkMode={false} />
-);
+const CustomErrorPage: NextPage<ErrorProps> = (props) => {
+  console.log(props)
+
+  return (
+    <NextErrorComponent {...props} withDarkMode={false} />
+  )
+};
 
 export default CustomErrorPage;
